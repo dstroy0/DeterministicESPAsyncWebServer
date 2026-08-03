d8048bcc63166e41d550c397146de2aa9a0b1620
dstroy0 (Douglas Quigg) <dquigg123@gmail.com>
2026-08-03 13:40:22 -0400

    test: pass the tls flag listener_add no longer defaults

--- test/test_session/test_session.c
+++ test/test_session/test_session.c
@@ -34,7 +34,7 @@ void setUp()
     set_millis(0);
     queue_stage_reset(); // clear any staged events from previous test
     proto_tcp_pool_init(NULL);
-    listener_add(0, 80, PROTO_HTTP);
+    listener_add(0, 80, PROTO_HTTP, PROTO_FALSE);
     for (int i = 0; i < MAX_CONNS; i++)
     {
         conn_pool[i].state = CONN_ACTIVE;
